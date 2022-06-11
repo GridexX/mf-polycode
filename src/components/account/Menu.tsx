a75f25b046c6da059afb821eaa6951eccaab0aba
esteban.baron <esteban.baron@etu.umontpellier.fr>
2022-06-11 20:03:29 +0000

    Resolve "Add account Menu component"

---
+++ src/components/account/Menu.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import { Box, Button, Stack, useTheme } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LockIcon from '@mui/icons-material/Lock';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useTranslation } from '../../lib/translations';
+
+import styles from '../../styles/components/account/Menu.module.css';
+import profileImage from '../../../public/images/profil_image.svg';
+
+export default function Menu() {
+  const { i18n } = useTranslation();
+
+  // import MUI theme
+  const theme = useTheme();
+
+  const [buttonSelected, setButtonSelected] = React.useState('profile');
+
+  const handleChange = (event: React.MouseEvent<HTMLElement>) => {
+    setButtonSelected((event.target as HTMLElement).id);
+  };
+
+  return (
+    <Box className={styles.container}>
+      <Box className={styles.innerContainer}>
+        {/* profile image */}
+        <Box className={styles.profileImage}>
+          <Image src={profileImage} />
+        </Box>
+        {/* navigation buttons */}
+        <Box
+          sx={{ color: theme.palette.text.primary }}
+          className={styles.navigationButtons}
+        >
+          <Stack direction="column" spacing={3}>
+            <Link href="/account/profile">
+              <Button
+                id="profile"
+                className={styles.button}
+                startIcon={<PersonIcon className={styles.icon} />}
+                color={buttonSelected === 'profile' ? 'primary' : 'inherit'}
+                onClick={handleChange}
+              >
+                {i18n.t('account.menu.profile')}
+              </Button>
+            </Link>
+            <Link href="/account/settings">
+              <Button
+                id="settings"
+                className={styles.button}
+                startIcon={<SettingsIcon className={styles.icon} />}
+                color={buttonSelected === 'settings' ? 'primary' : 'inherit'}
+                onClick={handleChange}
+              >
+                {i18n.t('account.menu.settings')}
+              </Button>
+            </Link>
+            <Link href="/account/teams">
+              <Button
+                id="teams"
+                className={styles.button}
+                startIcon={<PeopleAltIcon className={styles.icon} />}
+                color={buttonSelected === 'teams' ? 'primary' : 'inherit'}
+                onClick={handleChange}
+              >
+                {i18n.t('account.menu.teams')}
+              </Button>
+            </Link>
+            <Link href="/account/password">
+              <Button
+                id="password"
+                className={styles.button}
+                startIcon={<LockIcon className={styles.icon} />}
+                color={buttonSelected === 'password' ? 'primary' : 'inherit'}
+                onClick={handleChange}
+              >
+                {i18n.t('account.menu.password')}
+              </Button>
+            </Link>
+          </Stack>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
