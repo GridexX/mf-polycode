46be877bcea715d851267a0fbb15dd330f2a3954
esteban.baron <esteban.baron@etu.umontpellier.fr>
2022-06-23 15:46:56 +0000

    feat: Create navbar Menu

---
+++ src/components/navbar/Menu.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Box, IconButton, Typography, Stack, Link } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import Image from 'next/image';
+import { useTranslation } from '../../lib/translations';
+
+import polybunny from '../../../public/images/polybunny-navbar-menu.png';
+
+import styles from '../../styles/components/navbar/Menu.module.css';
+
+type Props = {
+  handleMenu: () => void;
+};
+
+export default function Menu({ handleMenu }: Props) {
+  const { i18n } = useTranslation();
+
+  // --- handle events ---
+  const logout = () => {
+    console.log('logout');
+  };
+
+  return (
+    <Box className={styles.container}>
+      {/* close button */}
+      <Box className={styles.closeIconContainer}>
+        <IconButton
+          size="large"
+          edge="end"
+          color="inherit"
+          onClick={handleMenu}
+        >
+          <CloseIcon className={styles.closeIcon} />
+        </IconButton>
+      </Box>
+
+      {/* links container */}
+      <Box className={styles.linksContainer}>
+        {/* links */}
+        <Stack direction="column" alignItems="center" spacing={2}>
+          <Link href="/account/profile">
+            <Typography className={styles.linkItem} variant="h1">
+              {i18n.t('components.navbar.menu.profile')}
+            </Typography>
+          </Link>
+          <Link href="/leaderboard">
+            <Typography className={styles.linkItem} variant="h1">
+              {i18n.t('components.navbar.menu.leaderboard')}
+            </Typography>
+          </Link>
+          <Link href="/account/settings">
+            <Typography className={styles.linkItem} variant="h1">
+              {i18n.t('components.navbar.menu.settings')}
+            </Typography>
+          </Link>
+        </Stack>
+
+        {/* logout */}
+        <Box className={styles.logoutContaier}>
+          <Link href="/">
+            <Typography
+              className={styles.linkItem}
+              variant="h1"
+              onClick={logout}
+            >
+              {i18n.t('components.navbar.menu.logout')}
+            </Typography>
+          </Link>
+        </Box>
+      </Box>
+
+      {/* polybunny image */}
+      <Box className={styles.imageContainer}>
+        <Image
+          src={polybunny}
+          alt="polybunny image"
+          layout="fill"
+          objectFit="contain"
+        />
+      </Box>
+    </Box>
+  );
+}
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   AppBar,
   Box,
@@ -10,9 +10,12 @@ import {
 } from '@mui/material';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import Image from 'next/image';
+
 import Polypoints from '../Polypoints';
 import NavBarLink from './NavBarLink';
-import styles from '../../styles/components/NavBar.module.css';
+import Menu from './Menu';
+
+import styles from '../../styles/components/navbar/NavBar.module.css';
 import { useTranslation } from '../../lib/translations';
 
 export default function NavBar() {
@@ -30,91 +33,90 @@ export default function NavBar() {
   const theme = useTheme();
   const { i18n } = useTranslation();
 
+  // --- states ---
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // --- handle events ---
+  const handleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-      <AppBar className={styles.transparent} position="static">
-        <Toolbar sx={{ backgroundColor: 'white', m: 1 }}>
-          <IconButton
-            size="small"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <Image src="/images/logo.png" width={72} height={72} />
-          </IconButton>
-          <Typography
-            variant="h4"
-            noWrap
-            component="div"
-            color={theme.palette.primary.main}
-            sx={{ display: { xs: 'none', sm: 'block' } }}
-          >
-            Poly<span style={{ color: 'black' }}>Code</span>
-          </Typography>
-          <Stack
-            spacing={{ lg: 10, xl: 16 }}
-            direction="row"
-            sx={{ ml: 18, display: { xs: 'none', md: 'none', lg: 'flex' } }}
-          >
-            <NavBarLink href="/practice">
-              {i18n.t('components.navbar.practice')}
-            </NavBarLink>
-            <NavBarLink href="/challenge">
-              {i18n.t('components.navbar.challenge')}
-            </NavBarLink>
-            <NavBarLink href="/certification">
-              {i18n.t('components.navbar.certification')}
-            </NavBarLink>
-          </Stack>
-
-          <Box sx={{ flexGrow: 1 }} />
-          <Stack
-            spacing={4}
-            direction="row"
-            sx={{ display: { xs: 'none', md: 'flex' } }}
-          >
-            {isLoggedIn && (
-              <>
-                <Polypoints points={polypoints} size="normal" />
-                <IconButton
-                  size="large"
-                  edge="end"
-                  // aria-controls={menuId}
-                  // aria-haspopup="true"
-                  // onClick={handleProfileMenuOpen}
-                  color="inherit"
-                >
-                  <MenuRoundedIcon
-                    fontSize="large"
-                    sx={{ color: theme.palette.primary.main }}
-                  />
-                </IconButton>
-              </>
-            )}
-            {!isLoggedIn && (
-              <span style={{ marginRight: '12px' }}>
-                <NavBarLink href="/sign-in">
-                  {i18n.t('components.navbar.sign-in')}
-                </NavBarLink>
-              </span>
-            )}
-          </Stack>
-
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+      {!isMenuOpen && (
+        <AppBar className={styles.transparent} position="static">
+          <Toolbar sx={{ backgroundColor: 'white', m: 1 }}>
+            {/* title */}
             <IconButton
-              size="large"
-              aria-label="show more"
-              // aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              // onClick={handleMobileMenuOpen}
+              size="small"
+              edge="start"
               color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2 }}
             >
-              <MenuRoundedIcon fontSize="large" />
+              <Image src="/images/logo.png" width={72} height={72} />
             </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
+            <Typography
+              variant="h4"
+              noWrap
+              component="div"
+              color={theme.palette.primary.main}
+              sx={{ display: { xs: 'none', sm: 'block' } }}
+            >
+              Poly<span style={{ color: 'black' }}>Code</span>
+            </Typography>
+
+            {/* stack of links */}
+            <Stack
+              spacing={{ lg: 10, xl: 16 }}
+              direction="row"
+              sx={{ ml: 18, display: { xs: 'none', md: 'none', lg: 'flex' } }}
+            >
+              <NavBarLink href="/practice">
+                {i18n.t('components.navbar.practice')}
+              </NavBarLink>
+              <NavBarLink href="/challenge">
+                {i18n.t('components.navbar.challenge')}
+              </NavBarLink>
+              <NavBarLink href="/certification">
+                {i18n.t('components.navbar.certification')}
+              </NavBarLink>
+            </Stack>
+
+            {/* separator */}
+            <Box sx={{ flexGrow: 1 }} />
+
+            {/* stack of icons */}
+            <Stack spacing={4} direction="row">
+              {isLoggedIn && (
+                <Box className={styles.iconsContainer}>
+                  <Polypoints points={polypoints} size="normal" />
+                  <IconButton
+                    className={styles.menuIcon}
+                    size="large"
+                    edge="end"
+                    color="inherit"
+                    onClick={handleMenu}
+                  >
+                    <MenuRoundedIcon
+                      fontSize="large"
+                      sx={{ color: theme.palette.primary.main }}
+                    />
+                  </IconButton>
+                </Box>
+              )}
+              {!isLoggedIn && (
+                <span style={{ marginRight: '12px' }}>
+                  <NavBarLink href="/sign-in">
+                    {i18n.t('components.navbar.sign-in')}
+                  </NavBarLink>
+                </span>
+              )}
+            </Stack>
+          </Toolbar>
+        </AppBar>
+      )}
+      {isMenuOpen && <Menu handleMenu={handleMenu} />}
       {/* Sould be used when the NavBar will be set to fixed */}
       {/* <Toolbar sx={{height: "114px"}} /> */}
     </>
